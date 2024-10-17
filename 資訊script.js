48f49db7cead52b272e507b37f5ba3931af4ef92
LanPaTone <JosephYeh40093929@gmail.com>
2024-10-18 00:04:49 +0800

    Add files via upload

---
+++ 資訊script.js
@@ -0,0 +1,5 @@
+// 這裡可以放置股市即時報價的代碼
+document.addEventListener('DOMContentLoaded', function() {
+    const ticker = document.getElementById('ticker');
+    ticker.innerHTML = '<p>即時股市報價：AAPL $145.00, GOOGL $2800.00, AMZN $3400.00</p>';
+});
